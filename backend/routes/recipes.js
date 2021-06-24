4125584cedd5409bf88d7fd3021cddb15a535cde
Marian Ščupák <scupak.m@gmail.com>
2021-06-25 00:22:14 +0200

    Recipe adding form progress

--- backend/routes/recipes.js
+++ backend/routes/recipes.js
@@ -33,53 +33,56 @@ router.route('/').post((req, res) => {
 });
 
 router.route('/add').post((req, res) => {
-    const { body } = req;
-    const { token, recipe } = body;
+    // const { body } = req;
+    // const { token, recipe } = body;
 
-    console.log(body);
+    console.log(req.body);
+    console.log(req.files);
 
-    if (token) {
-      fetch("http://localhost:5000/api/verify?token=" + token)
-        .then((res) => res.json())
-        .then((json) => {
-          if (json.success) {
-            let newRecipe = new Recipe();
+    res.send("finally");
 
-            newRecipe.name = recipe.name;
-            newRecipe.description = recipe.description;
-            newRecipe.ingredients = recipe.ingredients;
-            newRecipe.steps = recipe.steps;
-            newRecipe.userId = json.user.id;
+    // if (token) {
+    //   fetch("http://localhost:5000/api/verify?token=" + token)
+    //     .then((res) => res.json())
+    //     .then((json) => {
+    //       if (json.success) {
+    //         let newRecipe = new Recipe();
 
-            newRecipe.save((err, doc) => {
-              if (err) {
-                return res.send({
-                  success: false,
-                  message: "Error: Server error."
-                });
-              }
-              return res.send({
-                success: true,
-                recipe: doc
-              });
-            });
-          } else {
-            return res.send({
-              success: false,
-              message: "Error: Not authenticated."
-            });
-          }
-        })
-        .catch(e => {
-          console.log(e);
-        })
-    }
-    else {
-      return res.send({
-        success: false,
-        message: "Error: Not authenticated."
-      });
-    }
+    //         newRecipe.name = recipe.name;
+    //         newRecipe.description = recipe.description;
+    //         newRecipe.ingredients = recipe.ingredients;
+    //         newRecipe.steps = recipe.steps;
+    //         newRecipe.userId = json.user.id;
+
+    //         newRecipe.save((err, doc) => {
+    //           if (err) {
+    //             return res.send({
+    //               success: false,
+    //               message: "Error: Server error."
+    //             });
+    //           }
+    //           return res.send({
+    //             success: true,
+    //             recipe: doc
+    //           });
+    //         });
+    //       } else {
+    //         return res.send({
+    //           success: false,
+    //           message: "Error: Not authenticated."
+    //         });
+    //       }
+    //     })
+    //     .catch(e => {
+    //       console.log(e);
+    //     })
+    // }
+    // else {
+    //   return res.send({
+    //     success: false,
+    //     message: "Error: Not authenticated."
+    //   });
+    // }
 });
 
 module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const multer = require('multer');
 const mongoose = require('mongoose');
 
 require('dotenv').config();
@@ -7,8 +8,11 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
+const upload = multer({dest: 'upload/'});
+
 app.use(cors());
 app.use(express.json());
+app.use(upload.array('files'));
 
 const uri = process.env.ATLAS_URI;
 mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
